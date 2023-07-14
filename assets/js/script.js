26a3bc6438990d412c7397be094ecb6c5fc2b1d9
Carolina Lunas <carolinalunasfarah@gmail.com>
2023-07-14 15:07:27 -0400

    js start

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+let tasks = [
+    {id: 1, name: "Finish blueprints", completed: false},
+    {id: 2, name: "Draw embroidery project", completed: false},
+    {id: 3, name: "Find tattoo artist", completed: false}
+]
+
+let newTask = document.getElementById ("newTask")
+let addTask = document.getElementById ("addTask")
+let count = document.getElementById ("count")
+let done = document.getElementById ("done")
+let taskList = document.getElementsByClassName ("taskList")
+
